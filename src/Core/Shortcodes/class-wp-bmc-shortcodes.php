cfc3687d6a4b928c7c56585aeccfdbd0a1fa0c67
cubikean <hugochatigny@gmail.com>
2025-10-13 17:29:18 +0200

    Adjust CSS

--- src/Core/Shortcodes/class-wp-bmc-shortcodes.php
+++ src/Core/Shortcodes/class-wp-bmc-shortcodes.php
@@ -25,7 +25,7 @@ class WP_BMC_Shortcodes {
      */
     public function login_form() {
         if (WP_BMC_Auth::is_logged_in()) {
-            return '<div class="wp-bmc-message success wp-bmc-login-form">Vous êtes déjà connecté. <a href="' . home_url('/dashboard/') . '">Aller au tableau de bord</a></div>';
+            return '<script>window.location.href = "' . home_url('/dashboard/') . '";</script>';
         }
         
         return WP_BMC_Template_Loader::get_template_content('public/login-form');
@@ -36,7 +36,7 @@ class WP_BMC_Shortcodes {
      */
     public function register_form() {
         if (WP_BMC_Auth::is_logged_in()) {
-            return '<div class="wp-bmc-message success wp-bmc-login-form">Vous êtes déjà connecté. <a href="' . home_url('/dashboard/') . '">Aller au tableau de bord</a></div>';
+                return '<script>window.location.href = "' . home_url('/dashboard/') . '";</script>';
         }
         
         return WP_BMC_Template_Loader::get_template_content('public/register-form');
